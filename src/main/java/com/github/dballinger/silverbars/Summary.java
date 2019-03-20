23fd8c1d18dbbd2050742add445d4127bf84d7a6
Darren Ballinger <darren.ballinger@gmail.com>
2019-03-20 15:44:18 +0000

    Reduce duplication

--- src/main/java/com/github/dballinger/silverbars/Summary.java
+++ src/main/java/com/github/dballinger/silverbars/Summary.java
@@ -5,48 +5,43 @@ import com.google.common.collect.Ordering;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.github.dballinger.silverbars.OrderType.Buy;
 import static com.github.dballinger.silverbars.OrderType.Sell;
 
 public class Summary {
+    private static final Collector<Order, ?, Map<GBP, List<Order>>> groupByPrice = Collectors.groupingBy(Order::getPricePerUnit);
+    private static final Function<Map.Entry<GBP, List<Order>>, SummaryItem> toAggregatedSummaryItem = entry ->
+                                                                                                       new SummaryItem(
+                                                                                                        entry.getValue().stream().map(Order::getQty).reduce(Kilograms.ZERO, Kilograms.sum()),
+                                                                                                        entry.getKey()
+                                                                                                       );
+
     private final List<SummaryItem> sell;
     private final List<SummaryItem> buy;
-    private final Collector<Order, ?, Map<GBP, List<Order>>> groupByPrice = Collectors.groupingBy(Order::getPricePerUnit);
 
     public Summary(Collection<Order> orders) {
+        Stream<Order> sellOrders = orders.stream().filter(Sell.acceptedOrders());
+        Stream<Order> buyOrders = orders.stream().filter(Buy.acceptedOrders());
 
-        List<SummaryItem> aggregatedSell = orders.stream()
-                                            .filter(Sell.acceptedOrders())
-                                            .collect(groupByPrice)
-                                            .entrySet()
-                                            .stream()
-                                            .map(
-                                             entry ->
-                                              new SummaryItem(
-                                               entry.getValue().stream().map(Order::getQty).reduce(Kilograms.ZERO, Kilograms.sum()),
-                                               entry.getKey()
-                                              )
-                                            )
-                                            .collect(Collectors.toList());
-        List<SummaryItem> aggregatedBuy = orders.stream()
-                                            .filter(Buy.acceptedOrders())
-                                            .collect(groupByPrice)
-                                            .entrySet()
-                                            .stream()
-                                            .map(
-                                             entry ->
-                                              new SummaryItem(
-                                               entry.getValue().stream().map(Order::getQty).reduce(Kilograms.ZERO, Kilograms.sum()),
-                                               entry.getKey()
-                                              )
-                                            )
-                                            .collect(Collectors.toList());
+        List<SummaryItem> aggregatedSell = aggregateOrders(sellOrders);
+        List<SummaryItem> aggregatedBuy = aggregateOrders(buyOrders);
 
         sell = Ordering.from(Sell.summaryItemSort()).sortedCopy(aggregatedSell);
         buy = Ordering.from(Buy.summaryItemSort()).sortedCopy(aggregatedBuy);
+    }
+
+    private List<SummaryItem> aggregateOrders(Stream<Order> sellOrders) {
+        return sellOrders
+                .collect(groupByPrice)
+                .entrySet()
+                .stream()
+                .map(toAggregatedSummaryItem)
+                .collect(Collectors.toList());
     }
 
     @Override
